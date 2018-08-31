40c3eaa6e77d73b0091b481b6d28c67f50e3edeb
yona <04l.yona@gmail.com>
2018-08-31 02:05:23 -0400

    Add firebase as backend

---
+++ database/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyArT2n0g4o8RtJIaF6FcAHEHsKTC8j82_A",
+  authDomain: "first-ios-app-3537c.firebaseapp.com",
+  databaseURL: "https://first-ios-app-3537c.firebaseio.com",
+  projectId: "first-ios-app-3537c",
+  storageBucket: "first-ios-app-3537c.appspot.com",
+  messagingSenderId: "898624680174"
+};
+firebase.initializeApp(firebaseConfig);
