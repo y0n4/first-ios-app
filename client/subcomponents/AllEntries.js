36da0f0c34321698b447be4f57f90cb074e6a6dc
yona <04l.yona@gmail.com>
2018-09-02 16:31:04 -0400

    Render retrieved information with stylings

--- client/subcomponents/AllEntries.js
+++ client/subcomponents/AllEntries.js
@@ -5,23 +5,34 @@ import {
   View,
   ScrollView,
   Image,
-  Button
 } from 'react-native';
 
 export default function AllEntries(props) {
   console.log('this is PROPS', props.data);
   return (
-    <View>
-      <Text style={{textAlign: 'center'}}>*your past journal entries here*</Text>
-      <Text style={{textAlign: 'center'}}>currently in retrieve-info branch</Text>
+    <View style={{margin: 5}}>
+      <View style={{height: 24, backgroundColor: '#FFCFE0', padding: 2,}}>
+        <Text style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>{props.data.time}</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={{alignItems: 'flex-start'}}>
+        <Text>1. {props.data.entry1}</Text>
+        <Text>2. {props.data.entry2}</Text>
+        <Text>3. {props.data.entry3}</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    padding: 10,
+    backgroundColor: '#FFF6F9',
     alignItems: 'center',
+    borderColor: '#FFCFE0',
+    borderStyle: 'solid',
+    borderWidth: 2,
     // justifyContent: 'center',
   }
 });
