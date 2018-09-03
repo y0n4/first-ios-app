2fadb480b21528e662d59d6a709aab4aafd03eae
yona <04l.yona@gmail.com>
2018-09-02 21:03:10 -0400

    Some styling

--- client/subcomponents/AllEntries.js
+++ client/subcomponents/AllEntries.js
@@ -10,16 +10,14 @@ import {
 export default function AllEntries(props) {
   console.log('this is PROPS', props.data);
   return (
-    <View style={{margin: 5}}>
+    <View style={{marginVertical: 5, marginHorizontal: 2}}>
       <View style={{height: 24, backgroundColor: '#FFCFE0', padding: 2,}}>
         <Text style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>{props.data.time}</Text>
       </View>
       <View style={styles.container}>
-        <View style={{alignItems: 'flex-start'}}>
         <Text>1. {props.data.entry1}</Text>
         <Text>2. {props.data.entry2}</Text>
         <Text>3. {props.data.entry3}</Text>
-        </View>
       </View>
     </View>
   )
@@ -29,10 +27,9 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     backgroundColor: '#FFF6F9',
-    alignItems: 'center',
     borderColor: '#FFCFE0',
     borderStyle: 'solid',
+    alignItems: 'flex-start',
     borderWidth: 2,
-    // justifyContent: 'center',
   }
 });
